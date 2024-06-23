4c5a4d2a32d97e0cdfdb4f849a8adae6dfbeeca6
Roshan Gautam <roshangm01@gmail.com>
2024-06-23 19:52:24 +0100

    updated final

--- src/app/api/qr-generator/route.ts
+++ src/app/api/qr-generator/route.ts
@@ -9,15 +9,14 @@ const options: Partial<Options> = {
   height: 400,
   data: "https://app.leadsquik.com/add-leads-modal",
   // NOTE: png and jpg formats only.
-  image:
-    "https://www.citypng.com/public/uploads/preview/hd-meta-facebook-white-logo-png-116403446124ydgyipr3u.png",
-  dotsOptions: { type: "extra-rounded", color: "#242424" },
+  image: "https://app.leadsquik.com/logos/adaptive-icon.png",
+  dotsOptions: { type: "extra-rounded", color: "#2D5B5E" },
   backgroundOptions: {
     color: "#fff",
   },
-  cornersSquareOptions: { type: "extra-rounded", color: "#281728" },
+  cornersSquareOptions: { type: "extra-rounded", color: "#278572" },
   cornersDotOptions: {
-    color: "#928192",
+    color: "#2D5B5E",
   },
   imageOptions: {
     crossOrigin: "anonymous",
